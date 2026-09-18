ef4ffaa422b0690fa63956cc08738d8f35f7543c
Noel <noelmalnassy@gmail.com>
2026-09-18 15:33:49 +0200

    First commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hu.unideb.inf.kip_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens hu.unideb.inf.kip_javafx to javafx.fxml;
-    exports hu.unideb.inf.kip_javafx;
-}
